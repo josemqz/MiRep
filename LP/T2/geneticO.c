bedbedba8272e58fefc649afe45e12bbd7085486
josemqz <esteeselmaildejose@gmail.com>
2018-10-17 20:24:30 -0300

    dsfkl

--- LP/T2/geneticO.c
+++ LP/T2/geneticO.c
@@ -1,3 +1,4 @@
+
 #include "stdio.h"
 #include "stdlib.h"
 #include "time.h"
@@ -48,11 +49,14 @@ void* getRand(listNode* node){
 //a=rand%10
 //b=malloc
 //*((int*)b)
+
+
 void* generateNode(){
     listNode* node = (listNode*)malloc(sizeof(listNode));
     node = (listNode*)getRand(node);
     return node;
 }
+
 void next(void* List) {
     if (((linkList*)(List))->curr != ((linkList*)(List))->tail) {
         ((linkList*)(List))->curr = ((linkList*)(List))->curr->next;
@@ -76,22 +80,26 @@ void* generarSolucion(int largo){
 int getSize(void* List){
     return ((linkList*)(List))->listSize;
 }
+
 void moveToStart(void* List){
     ((linkList*)(List))->curr = ((linkList*)(List))->head;
     ((linkList*)(List))->pos = 0;
-
 }
 
 void* copiar(void* Lista){
     int size = ((linkList*)Lista)->listSize;
     linkList *L = (linkList*)malloc(size*sizeof(linkList));
-    L->curr = L->head = L->tail = (listNode*)malloc(sizeof(listNode));
     int i;
-    for (i = 0; i < ((linkList*)Lista)->listSize ;i++ ) {
-        L->curr = ((linkList*)Lista)->curr;
+    L->curr = L->head = L->tail = (listNode*)generateNode();
+    L->curr->next = NULL;
+    for (i = 1; i < size; i++) {
+        L->tail->next = (listNode*)generateNode();
+        L->tail = L->tail->next;
     }
+    L->listSize = size;
     return L;
 }
+
 
 void borrar(void* Lista){
     listNode* node = ((linkList*)Lista)->head;
@@ -108,9 +116,11 @@ void borrar(void* Lista){
 }
 
 
-
 void imprimirSolucion(void* Lista) {
-int i;
+
+    moveToStart(Lista);
+
+    int i;
     for (i = 0; i<((linkList*)(Lista))->listSize; i++) {
 
          if (((linkList*)(Lista))->curr->type == 'c') {
@@ -121,16 +131,154 @@ int i;
         }
         next((linkList*) Lista);
     }
+}
+
+
+void moveToPos(int pos, linkList* List) {
+    moveToStart(List);
+    while(List->pos != pos){
+        next(List);
+    }
+}
+
+
+void cruceMedio(void* Lista1, void* Lista2){
+
+    int a = (((linkList*)(Lista1))->listSize)/2-1; //elemento posterior a mitad
+    int b = (((linkList*)(Lista2))->listSize)/2-1;
+
+    moveToPos(a,((linkList*)(Lista1)));
+    moveToPos(b,((linkList*)(Lista2)));
+
+    listNode* P1 = ((linkList*)Lista1)->curr->next;
+
+    ((linkList*)Lista1)->curr->next = ((linkList*)Lista2)->curr->next;
+    ((linkList*)Lista2)->curr->next = P1;
+
+    listNode* H1 = ((linkList*)(Lista1))->head;
+    listNode* H2 = ((linkList*)(Lista2))->head;
+
+    ((linkList*)Lista1)->head = H2;
+    ((linkList*)Lista2)->head = H1;
+
+    int nSize = (((linkList*)Lista1)->listSize + ((linkList*)Lista2)->listSize)/2;
+
+    ((linkList*)Lista1)->listSize = nSize;
+    ((linkList*)Lista2)->listSize = nSize;
+
+    imprimirSolucion(Lista1);
+    printf("diferenciar las dos\n");
+    imprimirSolucion(Lista2);
+};
+
+void mutacionRand(void* Lista){
+    char* a[] = {"i","c","b"};
+    char* b[] = {"A","B","C","D","E","F"};
+
+    int domo = rand() % ((linkList*)Lista)->listSize;
+    moveToPos(domo,((linkList*)Lista));
+
+    ((linkList*)Lista)->curr->type = *a[rand()%3];
+
+    int flag = 1;
+
+    if (((linkList*)Lista)->curr->type == 'i'){
+        int* data2 = (int*)malloc(sizeof(int));
+        while(flag != 0){
+            *data2 = rand()%10;
+            if (*data2 != *((int*)((linkList*)Lista)->curr->data)) {
+                free(((linkList*)Lista)->curr->data);
+                ((linkList*)Lista)->curr->data = (int*)malloc(sizeof(int));
+                ((linkList*)Lista)->curr->data = data2;
+                flag = 0;
+            }
+        }
+    }
+
+    else if (((linkList*)Lista)->curr->type == 'c') {
+     	char* data2 = (char*)malloc(sizeof(char));
+        while(flag != 0){
+            *data2 = *b[rand()%6];
+            if (*data2 != *((char*)((linkList*)Lista)->curr->data)) {
+                free(((linkList*)Lista)->curr->data);
+                ((linkList*)Lista)->curr->data = (char*)malloc(sizeof(char));
+                ((linkList*)Lista)->curr->data = data2;
+                flag = 0;
+            }
+        }
+    }
+
+    else{
+        int* data2 = (int*)malloc(sizeof(int));
+        while(flag != 0){
+            *data2 = rand()%2;
+            if (*data2 != *((int*)((linkList*)Lista)->curr->data)) {
+                free(((linkList*)Lista)->curr->data);
+                ((linkList*)Lista)->curr->data = (int*)malloc(sizeof(int));
+                ((linkList*)Lista)->curr->data = data2;
+                flag = 0;
+            }
+        }
+    }
+}
+
+
+void mutacionTipo(void* Lista){
+    char* b[] = {"A","B","C","D","E","F"};
+
+    int domo = rand() % ((linkList*)Lista)->listSize;
+    moveToPos(domo,((linkList*)Lista));
+
+    int flag = 1;
+
+    if (((linkList*)Lista)->curr->type == 'i'){
+        int* data2 = (int*)malloc(sizeof(int));
+        while(flag != 0){
+            *data2 = rand()%10;
+            if (*data2 != *((int*)((linkList*)Lista)->curr->data)) {
+                ((linkList*)Lista)->curr->data = data2;
+                flag = 0;
+            }
+        }
+    }
+
+    else if (((linkList*)Lista)->curr->type == 'c') {
+     	char* data2 = (char*)malloc(sizeof(char));
+        while(flag != 0){
+            *data2 = *b[rand()%6];
+            if (*data2 != *((char*)((linkList*)Lista)->curr->data)) {
+                ((linkList*)Lista)->curr->data = data2;
+                flag = 0;
+            }
+        }
+    }
+
+    else{
+        int* data2 = (int*)malloc(sizeof(int));
+        while(flag != 0){
+            *data2 = rand()%2;
+            if (*data2 != *((int*)((linkList*)Lista)->curr->data)) {
+                ((linkList*)Lista)->curr->data = data2;
+                flag = 0;
+            }
+        }
+    }
+}
+
+
+void cruceIntercalado(void* Lista1, void* Lista2){
+
+
+
+
+
+
+
+
+
 
 }
 
-void cruceMedio(void* Lista1, void* Lista2);
-
-void cruceIntercalado(void* Lista1, void* Lista2);
-
-void mutacionRand(void* Lista);
-
-void mutacionTipo(void* Lista);
 
 int evaluacionLista(int (*fun)(void*), void* Lista);
 
